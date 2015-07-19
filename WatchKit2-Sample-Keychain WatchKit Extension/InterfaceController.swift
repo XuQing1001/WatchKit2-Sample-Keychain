59334ed0b1b57857edeb3dfd793b79ca29aef7b5
XuQing1001 <xq2417@gmail.com>
2015-07-19 16:31:41 +0800

    Initial Commit

---
+++ WatchKit2-Sample-Keychain WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchKit2-Sample-Keychain WatchKit Extension
+//
+//  Created by XuQing on 15/7/19.
+//  Copyright © 2015年 XuQing1001. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ WatchKit2-Sample-Keychain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchKit2-Sample-Keychain
+//
+//  Created by XuQing on 15/7/19.
+//  Copyright © 2015年 XuQing1001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
